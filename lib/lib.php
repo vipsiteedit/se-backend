2a5c1b06f08e76ca4eeb94a6c5cd826fa6d70256
Sergey Schelkonogov <vipsiteedit@gmail.com>
2024-11-20 21:25:33 +0300

    autoloader for lib

--- lib/lib.php
+++ lib/lib.php
@@ -1,6 +1,6 @@
 <?php
 
-function __autoload($psClassName)
+function seAutoLoad($psClassName)
 {
     if (file_exists(dirname(__file__) . '/classes/' . $psClassName . '.class.php')) {
         require_once dirname(__file__) . '/classes/' . $psClassName . '.class.php';
@@ -13,10 +13,12 @@ function __autoload($psClassName)
             while (false !== ($file = readdir($handle))) {
                 if (
                     is_dir(dirname(__file__) . '/plugins/' . $file)
-                    && strpos($file, "plugin_") !== false && strpos(strtolower($psClassName), $file) !== false
+                    && strpos($file, 'plugin_') !== false && strpos(strtolower($psClassName), $file) !== false
                 ) {
-                    if (file_exists(dirname(__file__) . '/plugins/' . $file . '/' . strtolower($psClassName) . '.class.php'))
+                    if (file_exists(dirname(__file__) . '/plugins/' . $file . '/' . strtolower($psClassName) . '.class.php')) {
                         require_once dirname(__file__) . '/plugins/' . $file . '/' . strtolower($psClassName) . '.class.php';
+                    }
+
                 }
             }
         }
@@ -24,16 +26,18 @@ function __autoload($psClassName)
     }
 }
 
+spl_autoload_register('seAutoLoad');
+
 setlocale(LC_ALL, 'ru_RU.CP1251');
 
-require_once dirname(__FILE__) . "/yaml/seYaml.class.php";
-require_once dirname(__FILE__) . "/lib_utf8.php";
-require_once dirname(__FILE__) . "/lib_nav.php";
-require_once dirname(__FILE__) . "/lib_function.php";
-require_once dirname(__FILE__) . "/lib_se_function.php";
-require_once dirname(__FILE__) . "/lib_safe_function.php";
-require_once dirname(__FILE__) . "/lib_images.php";
-require_once dirname(__FILE__) . "/lib_simpleXml.php";
-require_once dirname(__FILE__) . "/lib_voting.php";
-require_once dirname(__FILE__) . "/lib_rating.php";
-require_once dirname(__FILE__) . "/PHPMailer.php";
+require_once dirname(__FILE__) . '/yaml/seYaml.class.php';
+require_once dirname(__FILE__) . '/lib_utf8.php';
+require_once dirname(__FILE__) . '/lib_nav.php';
+require_once dirname(__FILE__) . '/lib_function.php';
+require_once dirname(__FILE__) . '/lib_se_function.php';
+require_once dirname(__FILE__) . '/lib_safe_function.php';
+require_once dirname(__FILE__) . '/lib_images.php';
+require_once dirname(__FILE__) . '/lib_simpleXml.php';
+require_once dirname(__FILE__) . '/lib_voting.php';
+require_once dirname(__FILE__) . '/lib_rating.php';
+require_once dirname(__FILE__) . '/PHPMailer.php';
